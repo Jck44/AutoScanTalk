4184f0ed7675caa6c1528ce55887727cf241e90b
Jck44 <ryouwrath@gmail.com>
2019-08-24 19:32:24 +0200

    #9 Added a basic start activity with navigation to a basic action page (including back navigation)

---
+++ app/src/main/java/com/jck44/autoscantalk/ActionPage_4.kt
@@ -0,0 +1,12 @@
+package com.jck44.autoscantalk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActionPage_4 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_action_page_4)
+    }
+}
---
+++ app/src/main/java/com/jck44/autoscantalk/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.jck44.autoscantalk
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+        val message = "Test"
+        val intent = Intent(this, ActionPage_4::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+}
