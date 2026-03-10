87c7c9c03fb45dba75ac9d5462c668eb4d413ff8
andreas.kratzer <jck44@outlook.com>
2026-03-10 09:48:36 +0100

    Chore: Bump version to 0.9.6
    This commit increments the application versioning for the next release.
    
    Key changes:
    - **Build Configuration**:
        - Updated `versionCode` to `26`.
        - Updated `versionName` to `0.9.6`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.andreas_kratzer.ghosttalk"
         minSdk = 35
         targetSdk = 36
-        versionCode = 25
-        versionName = "0.9.5"
+        versionCode = 26
+        versionName = "0.9.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
